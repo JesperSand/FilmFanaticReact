30ca8c7882040c4beb2fd79061d64895a1d389a4
Jesper Sand <jespersand@jespers-air.home>
2024-11-10 01:39:00 +0100

    fixed actor detail screen

--- src/components/ActorDetailPage.jsx
+++ src/components/ActorDetailPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { useParams, Link } from 'react-router-dom'; 
 import axios from 'axios';
 import './styles/ActorDetailPage.css';
 
@@ -13,6 +13,8 @@ function ActorDetailPage() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [isBioExpanded, setIsBioExpanded] = useState(false);
+  const [showAll, setShowAll] = useState(false); 
+  const [moviesToDisplay, setMoviesToDisplay] = useState(6); // Default to 6 movies
 
   const MAX_BIO_LENGTH = 300;
 
@@ -39,10 +41,39 @@ function ActorDetailPage() {
     fetchActorDetails();
   }, [actorId]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width >= 1070) {
+        setMoviesToDisplay(6); // Show 6 movies on large screens
+      } else if (width >= 900) {
+        setMoviesToDisplay(5); // Show 5 movies on medium screens
+      } else if (width >= 740) {
+        setMoviesToDisplay(4); // Show 4 movies on smaller screens
+      } else if (width >= 580) {
+        setMoviesToDisplay(3); // Show 3 movies on smaller screens
+      } else {
+        setMoviesToDisplay(2); // Show 2 movies on extra small screens
+      }
+    };
+
+    // Set the initial value of movies to display on page load
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const toggleBio = () => setIsBioExpanded(!isBioExpanded);
+  const toggleShowAll = () => setShowAll(!showAll); 
 
   if (loading) return <p>Loading actor details...</p>;
   if (error) return <p>{error}</p>;
+
+  const moviesToShow = showAll ? movies : movies.slice(0, moviesToDisplay);
 
   return (
     <div className="actor-detail">
@@ -72,9 +103,13 @@ function ActorDetailPage() {
         <div className="actor-films">
           <div className="actor-films-header">
             <h2>Known For</h2>
+            {/* Show All Button */}
+            <button onClick={toggleShowAll} className="show-all-button">
+              {showAll ? 'Show Less' : 'Show All'}
+            </button>
           </div>
           <div className="films-list">
-            {movies.map((movie) => (
+            {moviesToShow.map((movie) => (
               <div key={movie.id} className="film-item">
                 <Link to={`/movie/${movie.id}`}>
                   <img
